e9e2fab56a63b020c2ed033d3cf6a1dcae98ca97
Michiru Morishita <kokser.merio@gmail.com>
2022-07-28 17:27:06 +0800

    add printing for compiled model

--- bus_generator.py
+++ bus_generator.py
@@ -7,10 +7,31 @@ import logging
 import sys
 
 from systemrdl import RDLCompiler, RDLCompileError
+from systemrdl import RDLListener, RDLWalker
+from systemrdl.node import FieldNode
 from peakrdl_regblock import RegblockExporter
 from peakrdl_regblock.cpuif.axi4lite import AXI4Lite_Cpuif
 
 __version__ = '0.1.0'
+
+
+class ModelPrintingListener(RDLListener):
+    def __init__(self):
+        self.indent = 0
+
+    def enter_Component(self, node):
+        if not isinstance(node, FieldNode):
+            print('\t'*self.indent, node.get_path_segment())
+            self.indent += 1
+
+    def enter_Field(self, node):
+        bit_range_str = '[%d:%d]' % (node.high, node.low)
+        sw_access_str = 'sw=%s' % node.get_property('sw').name
+        print('\t'*self.indent, bit_range_str, node.get_path_segment(), sw_access_str)
+
+    def exit_Component(self, node):
+        if not isinstance(node, FieldNode):
+            self.indent -= 1
 
 
 def parse_arguments(argv=None) -> argparse.Namespace:
@@ -18,22 +39,25 @@ def parse_arguments(argv=None) -> argparse.Namespace:
     # Input and output
     parser = argparse.ArgumentParser()
     parser.add_argument('input',
-                        help='Read input from specified file(s)',
+                        help='read input from specified file(s)',
                         nargs='+')
+    parser.add_argument('-p', '--print',
+                        help='print model heriarchy',
+                        action='store_true')
     parser.add_argument('-o', '--output',
-                        help='Output folder')
+                        help='write output to specified folder')
     # By default, the logging level is set to WARNING, which means all
     # warning, error and critical error messages will be shown.
     # Using -q will set the logging level to ERROR, this will filter all
     # warning and lower priority messages.
     parser.add_argument('-q', '--quiet',
-                        help='Show only critical and errors',
+                        help='show only critical and errors',
                         dest='verbosity',
                         action='store_const',
                         const=logging.ERROR,
                         default=logging.WARNING)
     parser.add_argument('-v', '--verbose',
-                        help='Show almost all messages, excluding debug messages',
+                        help='show almost all messages, excluding debug messages',
                         dest='verbosity',
                         action='store_const',
                         const=logging.INFO)
@@ -74,13 +98,21 @@ def main(argv=None):
         # A compilation error occurred. Exit with error code
         sys.exit(1)
 
+    # Print
+    if args.print:
+        walker = RDLWalker(unroll=True)
+        listener = ModelPrintingListener()
+        walker.walk(root, listener)
+
     # Export a SystemVerilog implementation
-    exporter = RegblockExporter()
-    exporter.export(
-        root, args.output,
-        cpuif_cls=AXI4Lite_Cpuif
-    )
+    if args.output is not None:
+        exporter = RegblockExporter()
+        exporter.export(
+            root, args.output,
+            cpuif_cls=AXI4Lite_Cpuif
+        )
 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
